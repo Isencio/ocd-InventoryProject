d9edb1e1e4c4c0db89e0e07af3b4ffcc9bc3d27c
Nicholai <ladislalouise31@gmail.com>
2025-03-19 15:50:11 +0800

    Implemented editable rows in StockCards table

--- src/components/SPMS/OfficeSuppliesPage/StockCardsPage/StockCardsPage.js
+++ src/components/SPMS/OfficeSuppliesPage/StockCardsPage/StockCardsPage.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './StockCardsPage.css';
 import logo from '../../../../Assets/OCD-main.jpg';
 
 const StockCardsPage = () => {
     const navigate = useNavigate();
+    const tableRef = useRef(null);
 
     const onBack = () => {
         navigate(-1);
@@ -14,13 +15,48 @@ const StockCardsPage = () => {
         console.log('Export button clicked');
     };
 
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            addNewRow();
+        }
+    };
+
+    const addNewRow = () => {
+        const table = tableRef.current;
+        const newRow = table.insertRow(-1);
+
+        // Create cells for the new row
+        const columns = [
+            { editable: true }, // DATE
+            { editable: true }, // REFERENCE
+            { editable: true }, // RECEIPT Qty
+            { editable: true }, // RECEIPT Unit Cost
+            { editable: true }, // RECEIPT Total Cost
+            { editable: true }, // BALANCE Qty
+            { editable: true }, // BALANCE Unit Cost
+            { editable: true }, // BALANCE Total Cost
+            { editable: true }, // ISSUE Qty
+            { editable: true }, // ISSUE Office
+            { editable: true }, // No. of Days to Consume
+        ];
+
+        columns.forEach((column) => {
+            const cell = newRow.insertCell();
+            if (column.editable) {
+                cell.contentEditable = true;
+                cell.addEventListener('keydown', handleKeyDown); // Add event listener to new cells
+            }
+        });
+    };
+
     return (
         <div className="stock-cards-container">
             <button className="return-button" onClick={onBack}> &larr; </button>
             <div className="stock-cards-header">
                 <h1>Stock Card</h1>
                 <div className="table-container">
-                    <table>
+                    <table ref={tableRef}>
                         <thead>
                             <tr>
                                 <th>DATE</th>
@@ -32,7 +68,7 @@ const StockCardsPage = () => {
                             </tr>
                             <tr>
                                 <th></th> {/* Empty for DATE */}
-                                <th></th> {/* Empty for REFERENCE */} 
+                                <th></th> {/* Empty for REFERENCE */}
                                 {/* Sub-columns for RECEIPT */}
                                 <th>Qty</th>
                                 <th>Unit Cost</th>
@@ -48,21 +84,22 @@ const StockCardsPage = () => {
                             </tr>
                         </thead>
                         <tbody>
+                            {/* Initial editable row */}
                             <tr>
-                                <td>2023-10-01</td>
-                                <td>REF123</td>
+                                <td contentEditable="true" onKeyDown={handleKeyDown}></td>
+                                <td contentEditable="true" onKeyDown={handleKeyDown}></td>
                                 {/* Receipt Data */}
-                                <td>100</td>
-                                <td>$10</td>
-                                <td>$1000</td>
+                                <td contentEditable="true" onKeyDown={handleKeyDown}></td>
+                                <td contentEditable="true" onKeyDown={handleKeyDown}></td>
+                                <td contentEditable="true" onKeyDown={handleKeyDown}></td>
                                 {/* Balance Data */}
-                                <td>200</td>
-                                <td>$10</td>
-                                <td>$2000</td>
+                                <td contentEditable="true" onKeyDown={handleKeyDown}></td>
+                                <td contentEditable="true" onKeyDown={handleKeyDown}></td>
+                                <td contentEditable="true" onKeyDown={handleKeyDown}></td>
                                 {/* Issue Data */}
-                                <td>50</td>
-                                <td>Office A</td>
-                                <td>30</td> {/* No. of Days to Consume */}
+                                <td contentEditable="true" onKeyDown={handleKeyDown}></td>
+                                <td contentEditable="true" onKeyDown={handleKeyDown}></td>
+                                <td contentEditable="true" onKeyDown={handleKeyDown}></td>
                             </tr>
                         </tbody>
                     </table>
@@ -70,9 +107,7 @@ const StockCardsPage = () => {
             </div>
             <button className="export-button" onClick={onExport}>Export</button>
             <div className="right-image-section">
-                <img src={logo} 
-                alt="OCD logo" 
-                className="vertical-OCD-image" />
+                <img src={logo} alt="OCD logo" className="vertical-OCD-image" />
             </div>
         </div>
     );
