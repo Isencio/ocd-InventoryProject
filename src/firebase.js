fe9114bbbb5b57cd6ec26dfcd4178695edde1d01
Nicholai <ladislalouise31@gmail.com>
2025-04-02 09:47:31 +0800

    reconfigured Firebase configus to use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,17 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6ugHGaOl6kY_zKyw0x0QqYviqNT367JA",
-  authDomain: "ocdncr-proj.firebaseapp.com",
-  projectId: "ocdncr-proj",
-  storageBucket: "ocdncr-proj.firebasestorage.app",
-  messagingSenderId: "353921815542",
-  appId: "1:353921815542:web:879dfff97e58cee82fadd7",
-  measurementId: "G-PWS2FDW6RR"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
 export { auth, db };
