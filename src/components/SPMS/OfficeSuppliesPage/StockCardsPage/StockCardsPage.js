c639d3c05a861f9abb2db53d4096e2cab7d36fff
Nicholai <ladislalouise31@gmail.com>
2025-04-03 11:17:53 +0800

    Add loading and error modal to StockCardsPage

--- src/components/SPMS/OfficeSuppliesPage/StockCardsPage/StockCardsPage.js
+++ src/components/SPMS/OfficeSuppliesPage/StockCardsPage/StockCardsPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as XLSX from 'xlsx';
 import './StockCardsPage.css';
